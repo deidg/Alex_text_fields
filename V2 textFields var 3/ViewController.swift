74fd7526799f949ddbe851a090e848b63e71b7d4
deidg <64797316+deidg@users.noreply.github.com>
2023-03-05 20:55:50 +0300

    Initial Commit

---
+++ V2 textFields var 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  V2 textFields var 3
+//
+//  Created by Alex on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
