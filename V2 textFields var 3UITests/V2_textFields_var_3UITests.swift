a8db4c817863c868b185794fc9f78cdd7d74a0eb
deidg <64797316+deidg@users.noreply.github.com>
2023-03-16 18:45:41 +0300

    commit 1.4.1 UI1 cleaned

--- V2 textFields var 3UITests/V2_textFields_var_3UITests.swift
+++ V2 textFields var 3UITests/V2_textFields_var_3UITests.swift
@@ -8,77 +8,42 @@
 import XCTest
 
 final class V2_textFields_var_3UITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-     
+        
     }
-
+    
     override func tearDownWithError() throws {
     }
-
-    func testExample() throws {
+    
+    func testOnlyChar() throws {
         let app = XCUIApplication()
         app.launch()
-                
         XCUIApplication().scrollViews.otherElements.textFields["wwwww-ddddd"].tap()
-        
-//        let wwwwwDddddTextField = app.scrollViews.otherElements.textFields["wwwww-ddddd"]
-        
-//        let key6 = app/*@START_MENU_TOKEN@*/.keys["4"]/*[[".keyboards.keys[\"4\"]",".keys[\"4\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-//        key6.tap()
+        //        let key6 = app/*@START_MENU_TOKEN@*/.keys["4"]/*[[".keyboards.keys[\"4\"]",".keys[\"4\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        //        key6.tap()
         let dKey = app/*@START_MENU_TOKEN@*/.keys["D"]/*[[".keyboards.keys[\"D\"]",".keys[\"D\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         dKey.tap()
-        
-//        dKey.tap()
-        
         let fKey = app/*@START_MENU_TOKEN@*/.keys["f"]/*[[".keyboards.keys[\"f\"]",".keys[\"f\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         fKey.tap()
-//        fKey.tap()
-        
         let eKey = app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         eKey.tap()
-//        eKey.tap()
-        
         let rKey = app/*@START_MENU_TOKEN@*/.keys["r"]/*[[".keyboards.keys[\"r\"]",".keys[\"r\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         rKey.tap()
-//        rKey.tap()
-        
         let gKey = app/*@START_MENU_TOKEN@*/.keys["g"]/*[[".keyboards.keys[\"g\"]",".keys[\"g\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         gKey.tap()
-//        gKey.tap()
-        
         let moreKey = app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
         moreKey.tap()
-//        moreKey.tap()
-        
         let key = app/*@START_MENU_TOKEN@*/.keys["3"]/*[[".keyboards.keys[\"3\"]",".keys[\"3\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         key.tap()
-//        key.tap()
-        
         let key2 = app/*@START_MENU_TOKEN@*/.keys["4"]/*[[".keyboards.keys[\"4\"]",".keys[\"4\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         key2.tap()
-//        key2.tap()
         app/*@START_MENU_TOKEN@*/.keys["5"]/*[[".keyboards.keys[\"5\"]",".keys[\"5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        
         let key3 = app/*@START_MENU_TOKEN@*/.keys["6"]/*[[".keyboards.keys[\"6\"]",".keys[\"6\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         key3.tap()
-//        key3.tap()
-        
         let key4 = app/*@START_MENU_TOKEN@*/.keys["7"]/*[[".keyboards.keys[\"7\"]",".keys[\"7\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         key4.tap()
-//        key4.tap()
-//        app.buttons["Return"].tap()
-        
         XCTAssert(true)
-        
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
 }
